48943fc35c40c0a3010ec6553c8044b0de7b38b1
LYDIA LOCKHART <LYDIA.LOCKHART4@GMAIL.COM>
2024-08-07 08:22:54 -0500

    change imageURL to imageUrl

--- controllers/clothingItems.js
+++ controllers/clothingItems.js
@@ -14,9 +14,9 @@ const createItem = (req, res) => {
   console.log(req);
   console.log(req.body);
 
-  const { name, weather, imageURL } = req.body;
+  const { name, weather, imageUrl } = req.body;
 
-  ClothingItem.create({ name, weather, imageURL })
+  ClothingItem.create({ name, weather, imageUrl })
     .then((item) => {
       console.log(item);
       res.send({ data: item });
@@ -58,9 +58,9 @@ const getItems = (req, res) => {
 
 const updateItem = (req, res) => {
   const { itemId } = req.params;
-  const { imageURL } = req.body;
+  const { imageUrl } = req.body;
 
-  ClothingItem.findByIdAndUpdate(itemId, { $set: { imageURL } })
+  ClothingItem.findByIdAndUpdate(itemId, { $set: { imageUrl } })
     .orFail()
     .then((item) => res.status(200).send({ data: item }))
     .catch((err) => {
