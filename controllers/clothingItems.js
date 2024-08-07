db96ffbf2986793ac15a2c2edefefcf5b3a497b3
LYDIA LOCKHART <LYDIA.LOCKHART4@GMAIL.COM>
2024-08-07 16:36:27 -0500

    continue making corrections based on eslinter suggestions

--- controllers/clothingItems.js
+++ controllers/clothingItems.js
@@ -11,7 +11,6 @@ const createItem = (req, res) => {
 
   ClothingItem.create({ name, weather, imageUrl, owner: req.user._id })
     .then((item) => {
-      console.log(item);
       res.send({ data: item });
     })
     .catch((err) => {
