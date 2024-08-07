03a5e1992f9c3553f8b18bf695ffbe01ad185d69
LYDIA LOCKHART <LYDIA.LOCKHART4@GMAIL.COM>
2024-08-07 09:34:23 -0500

    final edits

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
-// const mainRouter = require("./routes/index");
 const { PORT = 3001 } = process.env;
 const app = express();
 
-// mongoose.set("strictQuery", true);
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
   .then(() => {
@@ -12,19 +10,17 @@ mongoose
   })
   .catch(console.error);
 
-const routes = require("./routes");
-app.use(express.json());
-app.use(routes);
-
 //middleware
 app.use((req, res, next) => {
   req.user = {
-    _id: "66b119770b83d2013e525d7d", // paste the _id of the test user created in the previous step
+    _id: "66b119770b83d2013e525d7d",
   };
   next();
 });
 
-// app.use("/", mainRouter);
+const routes = require("./routes");
+app.use(express.json());
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
