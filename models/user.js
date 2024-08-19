171f3660ac61bf447dcefc21937b0ac8498ef06d
LYDIA LOCKHART <LYDIA.LOCKHART4@GMAIL.COM>
2024-08-19 12:50:33 -0500

    add user email and password to userSchema

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const { default: isEmail } = require("validator/lib/isEmail");
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true, minlength: 2, maxlength: 30 },
@@ -13,6 +14,19 @@ const userSchema = new mongoose.Schema({
       message: "You must enter a valid URL",
     },
   },
+  email: {
+    type: String,
+    required: true,
+    minlength: 10,
+    maxlength: 30,
+    validate: {
+      validator(value) {
+        return validator.isEmail(value);
+      },
+      message: "You must enter a valid email address",
+    },
+  },
+  password: { type: String, required: true, minlength: 8, maxlength: 30 },
 });
 
 module.exports = mongoose.model("user", userSchema);
