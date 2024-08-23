d0574c464113c903b80c12c863c6a93b5c92794b
LYDIA LOCKHART <LYDIA.LOCKHART4@GMAIL.COM>
2024-08-23 15:10:46 -0500

    develop createItem function

--- controllers/clothingItems.js
+++ controllers/clothingItems.js
@@ -1,5 +1,7 @@
 const { BAD_REQUEST_ERROR_CODE } = require("../utils/errors");
 
+const clothingItem = require("../models/clothingItem");
+
 const getAllItems = (req, res) => {
   const { clothingItems } = req.body;
   req
@@ -18,6 +20,24 @@ const getAllItems = (req, res) => {
 };
 
 const createItem = (req, res) => {
+  const { name, imageUrl, weather } = req.body;
+  const { owner } = req.user._id;
+
+  clothingItem
+    .create(name, imageUrl, weather, owner)
+    .then(() => res.status(201).send(name, imageUrl, weather, owner))
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "ValidationError") {
+        return res
+          .status(BAD_REQUEST_ERROR_CODE)
+          .send({ message: "Invalid data" });
+      }
+      if (err.name === 11000) {
+        return res.status(409).send({ message: "duplicate error" });
+      }
+      return res.status(201).send(User);
+    });
   res.send("Item created");
 };
 
