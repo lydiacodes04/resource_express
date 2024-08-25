2cb47b1cb364463d62623b59065786dc7524a217
LYDIA LOCKHART <LYDIA.LOCKHART4@GMAIL.COM>
2024-08-25 07:51:16 -0500

    delete unnecessary comments

--- models/user.js
+++ models/user.js
@@ -39,36 +39,36 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.statics.findUserByCredentials = function findUserByCredentials(
-  email,
-  password,
-) {
-  return this.findOne({ email })
-    .select("+password")
-    .then((user) => {
-      if (!user) {
-        return Promise.reject(new Error("Incorrect email or password"));
-      }
-      return bcrypt
-        .compare(password, user.password)
-        .then((matched) => {
-          if (!matched) {
-            return Promise.reject(new Error("Incorrect email or password"));
-          }
-          return user;
-        })
-        .catch((err) => {
-          console.error(err);
-          if (err.code === 11000) {
-            return res
-              .status(CONFLICT_ERROR_CODE)
-              .send({ message: "Duplicate error" });
-          }
-          return res
-            .status(DEFAULT_ERROR_CODE)
-            .send({ message: "An error has occurred on the server." });
-        });
-    });
-};
+// userSchema.statics.findUserByCredentials = function findUserByCredentials(
+//   email,
+//   password,
+// ) {
+//   return this.findOne({ email })
+//     .select("+password")
+//     .then((user) => {
+//       if (!user) {
+//         return Promise.reject(new Error("Incorrect email or password"));
+//       }
+//       return bcrypt
+//         .compare(password, user.password)
+//         .then((matched) => {
+//           if (!matched) {
+//             return Promise.reject(new Error("Incorrect email or password"));
+//           }
+//           return user;
+//         })
+//         .catch((err) => {
+//           console.error(err);
+//           if (err.code === 11000) {
+//             return res
+//               .status(CONFLICT_ERROR_CODE)
+//               .send({ message: "Duplicate error" });
+//           }
+//           return res
+//             .status(DEFAULT_ERROR_CODE)
+//             .send({ message: "An error has occurred on the server." });
+//         });
+//     });
+// };
 
 module.exports = mongoose.model("user", userSchema);
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,6 @@
 const router = require("express").Router();
 const { getCurrentUser, updateProfile } = require("../controllers/users");
 
-// router.post("/", createUser);
-
-// router.get("/", getUsers);
-
-// router.get("/:userId", getCurrentUser);
-// router.patch("/:userId", updateProfile);
-
 const auth = require("../middlewares/auth");
 
 router.get("/me", auth, getCurrentUser);
