07d5799e4c4a51d818a1a4aa4bf2a6f3073659e7
LYDIA LOCKHART <LYDIA.LOCKHART4@GMAIL.COM>
2024-08-25 14:04:49 -0500

    remove catch block

--- models/user.js
+++ models/user.js
@@ -39,38 +39,23 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-// There is an issue here. Postman tests would not run unless this was commented out. I don't know where the problem is.
-
-// userSchema.statics.findUserByCredentials = function findUserByCredentials(
-//   email,
-//   password,
-// ) {
-//   return this.findOne({ email })
-//     .select("+password")
-//     .then((user) => {
-//       if (!user) {
-//         return Promise.reject(new Error("Incorrect email or password"));
-//       }
-//       return bcrypt
-//         .compare(password, user.password)
-//         .then((matched) => {
-//           if (!matched) {
-//             return Promise.reject(new Error("Incorrect email or password"));
-//           }
-//           return user;
-//         })
-//         .catch((err) => {
-//           console.error(err);
-//           if (err.code === 11000) {
-//             return res
-//               .status(CONFLICT_ERROR_CODE)
-//               .send({ message: "Duplicate error" });
-//           }
-//           return res
-//             .status(DEFAULT_ERROR_CODE)
-//             .send({ message: "An error has occurred on the server." });
-//         });
-//     });
-// };
+userSchema.statics.findUserByCredentials = function findUserByCredentials(
+  email,
+  password,
+) {
+  return this.findOne({ email })
+    .select("+password")
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error("Incorrect email or password"));
+      }
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          return Promise.reject(new Error("Incorrect email or password"));
+        }
+        return user;
+      });
+    });
+};
 
 module.exports = mongoose.model("user", userSchema);
