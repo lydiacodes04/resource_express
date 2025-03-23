539b8e9a0ab630a4bf7d3697f6587a0648975395
LYDIA LOCKHART <LYDIA.LOCKHART4@GMAIL.COM>
2025-03-22 19:29:06 -0500

    make updates according to Sprint 15 directions

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const app = express();
 
 const cors = require("cors");
 
+// const errorHandler = require("./middlewares/error-handler");
+
 app.use(cors());
 
 mongoose
@@ -22,6 +24,8 @@ const routes = require("./routes");
 app.use(express.json());
 
 app.use(routes);
+
+// app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
