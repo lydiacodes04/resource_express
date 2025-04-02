ef1d7eff5bb4df601bf15ac242878f287e580264
LYDIA LOCKHART <LYDIA.LOCKHART4@GMAIL.COM>
2025-04-02 18:56:08 -0500

    work on implementing error logger

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -28,8 +28,12 @@ const requestLogger = expressWinston.logger({
   ],
 });
 
-// error logger
 const errorLogger = expressWinston.errorLogger({
   transports: [new winston.transports.File({ filename: "error.log" })],
   format: winston.format.json(),
 });
+
+module.exports = {
+  requestLogger,
+  errorLogger,
+};
