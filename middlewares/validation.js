a987bf9769ac6ebbb7bab88251c50525e6aa0f88
LYDIA LOCKHART <LYDIA.LOCKHART4@GMAIL.COM>
2025-04-02 13:06:06 -0500

    work on joi validation

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,4 +1,68 @@
-const { celebrate, Joi } = require("celebrate");
+const { Joi, celebrate } = require("celebrate");
+const validator = require("validator");
+
+const validateClothingItem = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    imageUrl: Joi.string().required().uri(),
+  }),
+});
+
+const validateUserInfo = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    avatar: Joi.string().required().uri(),
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+});
+
+const validateUserLogin = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+});
+
+const validateUserID = celebrate({
+  body: Joi.object().keys({
+    userID: Joi.string().required().hex(24),
+  }),
+});
+
+const validateClothingItemID = celebrate({
+  body: Joi.object().keys({
+    clothingItemID: Joi.string().required().hex(24),
+  }),
+});
+
+const validateURL = (value, helpers) => {
+  if (validator.isURL(value)) {
+    return value;
+  }
+  return helpers.error("string.uri");
+};
+
+module.exports.validateCardBody = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30).messages({
+      "string.min": 'The minimum length of the "name" field is 2',
+      "string.max": 'The maximum length of the "name" field is 30',
+      "string.empty": 'The "name" field must be filled in',
+    }),
+
+    imageUrl: Joi.string().required().custom(validateURL).messages({
+      "string.empty": 'The "imageUrl" field must be filled in',
+      "string.uri": 'the "imageUrl" field must be a valid url',
+    }),
+  }),
+});
+
+module.exports.validateId = celebrate({
+  params: Joi.object().keys({
+    // ...
+  }),
+});
 
 router.post(
   "/posts",
