22dc205640a4edd17f78ac66e16cd3d97b17d7e3
LYDIA LOCKHART <LYDIA.LOCKHART4@GMAIL.COM>
2025-05-14 18:16:35 -0500

    make corrections

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,10 @@
 const router = require("express").Router();
 
-const { errors } = require("celebrate");
 const clothingItem = require("./clothingItems");
 
 const users = require("./users");
 
 const likes = require("./likes");
-
-
-const NotFoundError = require("../errors/not-found-error");
 
 const {
   validateUserInfo,
@@ -24,20 +20,5 @@ router.post("/signup", validateUserInfo, createUser);
 router.use("/items", clothingItem);
 router.use("/users", users);
 router.use("/items", likes);
-
-router.use(errors());
-
-router.use((req, res, next) => {
-  next(new NotFoundError("Requested resource not found"));
-  
-});
-
-router.use((err, req, res, next) => {
-  const { statusCode = 500, message } = err;
-
-  res.status(statusCode).send({
-    message: statusCode === 500 ? "An error occurred on the server" : message,
-  });
-});
 
 module.exports = router;
