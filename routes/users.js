e55f688ae87d7a5cec539aefc764567d3656ec9e
LYDIA LOCKHART <LYDIA.LOCKHART4@GMAIL.COM>
2025-05-15 19:45:58 -0500

    make corrections to users.js

--- routes/users.js
+++ routes/users.js
@@ -1,31 +1,13 @@
 const router = require("express").Router();
-const { celebrate, Joi } = require("celebrate");
-const {
-  getCurrentUser,
-  updateProfile,
-  createUser,
-} = require("../controllers/users");
+
+const { getCurrentUser, updateProfile } = require("../controllers/users");
 
 const { validateUpdateUser } = require("../middlewares/validation");
 
 const auth = require("../middlewares/auth");
 
+router.patch("/me", auth, validateUpdateUser, updateProfile);
+
 router.get("/me", auth, getCurrentUser);
-
-router.post(
-  "/signup",
-  celebrate({
-    body: {
-      email: Joi.string().required().email(),
-      password: Joi.string()
-        .required()
-        .min(8)
-        .pattern(/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*])/),
-    },
-  }),
-  createUser,
-);
-
-router.patch("/me", validateUpdateUser, auth, updateProfile);
 
 module.exports = router;
