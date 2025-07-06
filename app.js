cef139fec3df930b3777fb13ac6ffaca692eef34
LYDIA LOCKHART <LYDIA.LOCKHART4@GMAIL.COM>
2025-07-06 12:42:34 -0500

    update to local host 3004

--- app.js
+++ app.js
@@ -48,21 +48,3 @@ app.use(errorHandler); // general error handler
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-//project 15 approved!
-//commit 5/19
-// 5/19-5/31
-// no commits saved for 6/1 even though I did make commits to the branch I was working on
-// commits 6/2-6/30
-// commit 6/30, got search function to work and first item card to appear
-// commit 7/1, formatted Item Cards so that the resources display like cards. Worked on display of Main a bit as well.
-// commit 7/2, stopping point for the day. got /profile and / routes to work. Need to work on some formatting issues and messages to pop up for users.
-// commit 7/3
-// worked on final project: getting resource cards to appear horizontally,
-// getting footer to remain at the bottom of the page without absolute positioning.
-// "work on refining final project more; remove unnecessary components and code. improve alt tags"
-// commit 7/4
-// work on UI and responsive design
-//commit 7/4, 2.0
-// commit 7/4, final break before submit
-// back from break; time to finish
